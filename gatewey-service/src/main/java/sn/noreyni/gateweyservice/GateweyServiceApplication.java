5dcbf135840c204ae2b5b196c6e38e48f4aff609
ousmane DIOP <ousmanediopp268@gmail.com>
2025-06-21 12:57:39 +0000

    refactoring

--- gatewey-service/src/main/java/sn/noreyni/gateweyservice/GateweyServiceApplication.java
+++ gatewey-service/src/main/java/sn/noreyni/gateweyservice/GateweyServiceApplication.java
@@ -1,6 +1,5 @@
 package sn.noreyni.gateweyservice;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
@@ -10,14 +9,9 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GateweyServiceApplication {
-	@Value("application.test")
-	private static String values;
+
 
 	public static void main(String[] args) {
-		System.out.println("Starting Gatewey Service Application");
-		System.out.println("*************************");
-		System.out.println(values);
-		System.out.println("*************************");
 		SpringApplication.run(GateweyServiceApplication.class, args);
 	}
 
